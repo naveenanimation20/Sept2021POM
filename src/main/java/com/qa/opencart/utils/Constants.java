73229449f63acfd32ac46a80cd571159701d30d4
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-12-17 07:49:55 +0530

    fixed login test cases and deleted unused files

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Constants {
 
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
-	public static final String ACCOUNTS_PAGE_HEADER = "Your Store11";
+	public static final String ACCOUNTS_PAGE_HEADER = "Your Store";
 
 	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
 	
