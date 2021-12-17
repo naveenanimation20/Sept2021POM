02f961758b6a53dbc0b1a0a3328071ed3d16a61b
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-12-17 08:06:50 +0530

    updated default time out

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 
 	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
 	
-	public static final int DEFAULT_TIME_OUT = 7;
+	public static final int DEFAULT_TIME_OUT = 5;
 	public static final int IMAC_IMAGE_COUNT = 3;
 	public static final int MACBOOKPRO_IMAGE_COUNT = 4;
 	public static final int MACBOOKAIR_IMAGE_COUNT = 4;
